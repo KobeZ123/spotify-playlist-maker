766844e4d6a1d7a9876f6eeecfd1dbff098090b1
KobeZ123 <kobezhe@gmail.com>
2023-05-11 16:51:11 -0400

    implemented top artists display

---
+++ src/components/TopArtistsDisplay.tsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from "react";
+import useStore from "../stores/useStore";
+import { getTopArtistsLongTerm, getTopArtistsMediumTerm, getTopArtistsShortTerm } from "../api/loadData";
+
+import "../styles/top_artists_display.css";
+
+export default function TopArtistsDisplay() {
+
+    const token = useStore((state) => state.token);
+    // the user's top artists as a list of items
+    const [topArtistsList, setTopArtistsList] = useState<any[]>([]);
+    // the selected term for top artists 
+    const [selectedTerm, setSelectedTerm] = useState<string>("short_term");
+
+    // loads top artists when 
+    useEffect(() => {
+        if (token != null) {
+            if (selectedTerm === "short_term") {
+                getTopArtistsShortTerm(token, setTopArtistsList);
+            }
+            if (selectedTerm === "medium_term") {
+                getTopArtistsMediumTerm(token, setTopArtistsList);
+            }
+            if (selectedTerm === "long_term") {
+                getTopArtistsLongTerm(token, setTopArtistsList);
+            }
+        }
+    }, [selectedTerm]);
+
+    return (
+        <div className="top-artists-page-container">
+            <div className="term-tabs-container">
+                <button className={(selectedTerm === "short_term") ? "term-tab-selected" : "term-tab"}
+                    onClick={() => setSelectedTerm("short_term")}>Short Term</button>
+                <button className={(selectedTerm === "medium_term") ? "term-tab-selected" : "term-tab"}
+                    onClick={() => setSelectedTerm("medium_term")}>Medium Term</button>
+                <button className={(selectedTerm === "long_term") ? "term-tab-selected" : "term-tab"}
+                    onClick={() => setSelectedTerm("long_term")}>Long Term</button>
+            </div>
+            <div className="artists-container">
+                <section className="artist-cards-container">
+                    {topArtistsList.map((artist) => (
+                        <span className="artist-card-container" key={artist.name + "_card"}> 
+                            <p>{artist.name}</p>
+                            <img className="artist-img"
+                                src={(artist.images.length > 0) ? artist.images[0].url : ""}
+                                alt={`${artist.name}`}/>
+                        </span>
+                    ))}
+                </section>
+            </div>
+        </div>
+    )
+}
