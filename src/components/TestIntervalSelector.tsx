e64b0185d8830441134d896899d74afa6d2d3111
KobeZ123 <kobezhe@gmail.com>
2023-05-16 13:58:57 -0400

    began working on test interval selector component

---
+++ src/components/TestIntervalSelector.tsx
@@ -0,0 +1,18 @@
+
+export default function TestIntervalSelector() {
+    return (
+        <div>
+            <span>
+                <p>Minimum time</p>
+                <input>
+                </input>
+            </span>
+            <span>
+                <p>Maximum time</p>
+                <input>
+                </input>
+            </span>
+            
+        </div>
+    );
+}
