7d3278f8d864699f620db6433899688b1444efe6
KobeZ123 <kobezhe@gmail.com>
2023-05-16 20:00:50 -0400

    added millsecond time conversion functions

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -2,3 +2,18 @@
 export function getRandomInt(max: number) {
     return Math.floor(Math.random() * max);
 }
+
+// converts a duration (minutes and seconds) to milliseconds 
+export function durationToMilliseconds(minutes: number, seconds: number) {
+    return (seconds * 1000) + (minutes * 60000);
+}
+
+// given the milliseconds, return the lowest ms that rounds to the same second
+export function millisecondsLowerBound(milliseconds: number) {
+    return milliseconds - 499;
+}
+
+// given the milliseconds, return the highest ms that rounds to the same second
+export function millisecondsUpperBound(milliseconds: number) {
+    return milliseconds + 499;
+}
