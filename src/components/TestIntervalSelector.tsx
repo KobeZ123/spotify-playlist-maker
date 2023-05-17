55254f248ac26f7da4a72c4d57651b209bc330aa
KobeZ123 <kobezhe@gmail.com>
2023-05-16 21:08:47 -0400

    finished styling for interval input

--- src/components/TestIntervalSelector.tsx
+++ src/components/TestIntervalSelector.tsx
@@ -1,17 +1,38 @@
+import "../styles/interval_selector.css";
 
 export default function TestIntervalSelector() {
     return (
-        <div>
-            <span>
-                <p>Minimum time</p>
-                <input>
-                </input>
-            </span>
-            <span>
-                <p>Maximum time</p>
-                <input>
-                </input>
-            </span>
+        <div className="interval-selector-container">
+            <section className="bound-section-container">
+                <p className="section-label">Lower bound duration (minutes and seconds)</p>
+                <section className="bound-input-section"> 
+                    <span className="bound-input-span">
+                        <input className="input-number" type="number"/>
+                        <p className="unit-label">minutes</p>
+                    </span>
+                    <span className="bound-input-span">
+                        <input className="input-number" type="number"/>
+                        <p className="unit-label">seconds</p>
+                    </span>
+                </section>
+                
+                
+            </section>
+            <section className="bound-section-container">
+                <p className="section-label">Upper bound duration (minutes and seconds)</p>
+                <section className="bound-input-section"> 
+                    <span className="bound-input-span">
+                        <input className="input-number" type="number"/>
+                        <p className="unit-label">minutes</p>
+                    </span>
+                    <span className="bound-input-span">
+                        <input className="input-number" type="number"/>
+                        <p className="unit-label">seconds</p>
+                    </span>
+                </section>
+                
+            </section>
+            <button className="interval-submit-btn">SUBMIT</button>
             
         </div>
     );
