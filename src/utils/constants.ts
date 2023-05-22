76ea787917896bcad915a9bcc35e6329482a6bcf
KobeZ123 <kobezhe@gmail.com>
2023-05-22 17:31:12 -0400

    include parameter string constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,19 @@
-export const ARTISTS = "artists"
-export const GENRES = "genres"
-export const TRACKS = "tracks"
+export const ARTISTS = "artists";
+export const GENRES = "genres";
+export const TRACKS = "tracks";
+
+export const RECOMMENDATION_CONSTANTS = {
+    ACOUSTICNESS: "acousticness",
+    DANCEABILITY: "danceability",
+    ENERGY: "energy",
+    INSTRUMENTALNESS: "instrumentalness",
+    KEY:"key",
+    LIVENESS: "liveness",
+    LOUDNESS: "loudness",
+    MODE: "mode",
+    POPULARITY: "popularity",
+    SPEECHINESS: "speechiness",
+    TEMPO: "tempo",
+    TIME_SIGNATURE: "time_signature",
+    VALENCE: "valence",
+}
