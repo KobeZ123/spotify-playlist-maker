dd7211b2092229449300b3d2589d735cb325b5fe
KobeZ123 <kobezhe@gmail.com>
2023-06-09 12:40:43 -0400

    updating store to use localStorage

--- src/stores/useStore.ts
+++ src/stores/useStore.ts
@@ -1,37 +1,42 @@
-import { create } from 'zustand'
-type State = {
-  token: string | null
-}
+import create from 'zustand';
 
-// define the initial state
+type State = {
+  token: string | null;
+};
+
 const initialState: State = {
-  token: null
-}
+  token: localStorage.getItem('token') || null,
+};
 
 type StoreTypes = {
-  token: string | null
-  setToken: (token: string) => void
-  removeToken: () => void
-  reset: () => void
-  isTokenNull: () => boolean
-}
+  token: string | null;
+  setToken: (token: string) => void;
+  removeToken: () => void;
+  reset: () => void;
+  isTokenNull: () => boolean;
+};
 
 const useStore = create<StoreTypes>((set, get) => ({
-  ...initialState, 
+  ...initialState,
 
-  setToken: (token: string) => 
-    set(() => ({ token: token })),
+  setToken: (token: string) => {
+    localStorage.setItem('token', token);
+    set(() => ({ token: token }));
+  },
 
-  removeToken: () => 
-    set(() => ({ token: null })),
+  removeToken: () => {
+    localStorage.removeItem('token');
+    set(() => ({ token: null }));
+  },
 
   reset: () => {
-    set(initialState)
+    localStorage.removeItem('token');
+    set(initialState);
   },
 
   isTokenNull: () => {
     return get().token === null;
   },
-}))
+}));
 
 export default useStore;
