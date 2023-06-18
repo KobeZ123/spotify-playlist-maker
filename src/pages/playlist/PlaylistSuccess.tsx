0039aa96218917073aa1569be1ad4859462cf8e8
KobeZ123 <kobezhe@gmail.com>
2023-06-17 22:32:49 -0400

    playlist success page

--- src/pages/playlist/PlaylistSuccess.tsx
+++ src/pages/playlist/PlaylistSuccess.tsx
@@ -1,7 +1,35 @@
-export default function PlaylistSuccess() {
-    return (
-        <div>
+import { useState } from "react";
 
-        </div>
-    );
+export default function PlaylistSuccess() {
+  const [playlistOpened, setPlaylistOpened] = useState<boolean>(false);
+
+  return (
+    <div className="playlist-page-container">
+      <div className="playlist-page-content">
+        <h1>Interval Playlist Maker</h1>
+        <section>
+          <h3>Woohoo, your playlist was created!</h3>
+          <p>A playlist fit for you has been generated!</p>
+        </section>
+        {playlistOpened ? (
+          <div className="column-section">
+            <></>
+            <button
+              className="playlist-next-btn"
+              onClick={() => setPlaylistOpened(!playlistOpened)}
+            >
+              Close Playlist
+            </button>
+          </div>
+        ) : (
+          <button
+            className="playlist-back-btn"
+            onClick={() => setPlaylistOpened(!playlistOpened)}
+          >
+            View Playlist
+          </button>
+        )}
+      </div>
+    </div>
+  );
 }
