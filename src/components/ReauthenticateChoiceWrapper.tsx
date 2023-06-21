b2c154f97141e00672133b536906ab93f64a0256
KobeZ123 <kobezhe@gmail.com>
2023-06-21 15:53:09 -0400

    fixing syntax

--- src/components/ReauthenticateChoiceWrapper.tsx
+++ src/components/ReauthenticateChoiceWrapper.tsx
@@ -3,7 +3,7 @@ import { TOKEN_STRING } from "../utils/constants";
 import useStore from "../stores/useStore";
 import { getUserID } from "../api/loadData";
 import "../styles/reauthenticate.css";
-import "../styles/home.css"
+import "../styles/home.css";
 
 interface ChoiceWrapperProps {
   child_active: JSX.Element;
@@ -35,9 +35,5 @@ export default function ReauthenticateChoiceWrapper(props: ChoiceWrapperProps) {
     }
   }, [props.token]);
 
-  return sessionActive ? (
-    props.child_active
-  ) : (
-    props.child_error
-  );
+  return sessionActive ? props.child_active : props.child_error;
 }
