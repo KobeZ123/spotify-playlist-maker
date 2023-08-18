cd4e1f19d24688fcd4b45c26e4ba54259a27b490
KobeZ123 <kobezhe@gmail.com>
2023-08-18 17:53:43 -0400

    responsive styling to navigation bar

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,15 +1,24 @@
 import { Link } from "react-router-dom";
 import "../styles/layout.css";
 import spotify_logo from "../assets/spotify-logo.png";
+import hamburger_menu from "../assets/hamburger-menu.png";
+import { useState } from "react";
 
 export default function NavigationBar() {
+    const [menuOpen, setMenuOpen] = useState<boolean>(false);
+
     return (
-        <nav className="navbar">
-            <div className="navbar-logo">
-                <img src={spotify_logo} alt="Spotify Logo" style={{ height: "64px", maxHeight: "100%" }} />
-                <Link to="/" className="logo-link">Spotify Playlist Maker</Link>
+        <nav className={menuOpen? "navbar-menu-active": "navbar"}>
+            <div className="nav-responsive-block">
+                <div className="navbar-logo">
+                    <img src={spotify_logo} alt="Spotify Logo" className="spotify-icon" />
+                    <Link to="/" className="logo-link">Spotify Playlist Maker</Link>
+                </div>
+                <img src={hamburger_menu} alt="Menu" 
+                    className="hamburger-menu" 
+                    onClick={() => {setMenuOpen(!menuOpen)}}/>
             </div>
-            <ul className="navbar-items">
+            <ul className={menuOpen? "navbar-items-menu-active": "navbar-items"}>
                 <li className="nav-item">
                     <Link to="/" className="nav-link">Home</Link>
                 </li>
