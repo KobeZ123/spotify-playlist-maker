40ada071615af125545e35cdd891e6c131d9c185
KobeZ123 <kobezhe@gmail.com>
2023-08-18 16:22:17 -0400

    updating app branding

--- src/App.tsx
+++ src/App.tsx
@@ -17,21 +17,6 @@ import AboutPage from "./pages/AboutPage";
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>{process.env.REACT_APP_AUTH_ENDPOINT} + hit</p>
-      </header> */}
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Layout />}>
--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -2,12 +2,16 @@ import { Outlet } from "react-router-dom";
 import NavigationBar from "../components/NavigationBar";
 
 import "../styles/home.css";
+import Footer from "../components/Footer";
 
 export default function Layout() {
     return (
-        <div className="home-container">
+        <div className="app-container">
             <NavigationBar />
-            <Outlet />
+            <div className="app-content">
+                <Outlet />
+            </div>
+            <Footer />
         </div>
     );
 }
