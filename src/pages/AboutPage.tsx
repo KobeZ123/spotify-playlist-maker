374c747543be00bd21b5a31ff55e7fc99054d796
KobeZ123 <kobezhe@gmail.com>
2023-08-19 19:23:38 -0400

    reponsiveness to home and about pages

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,19 +1,24 @@
+import spotify_logo from "../assets/spotify-logo.png";
+import github_icon from "../assets/github.png";
+
 export default function AboutPage() {
   return (
     <div className="about-page-container">
-      <section>
+      
+        <img src={spotify_logo} alt="Spotify Logo" width={128} height={128} />
         <p className="about-page-p">
-          The Spotify Interval Playlist Maker is a web application that allows a
+          The Spotify Playlist Maker is a web application that allows a
           user to view their music listening profile and automatically generate
           an interval playlist based on recommendations selected by the user.
         </p>
         <a
           href="https://github.com/KobeZ123/spotify-playlist-maker"
-          className="get-started-btn no-decoration"
+          className="link-to-github-btn no-decoration"
         >
-          Link to GitHub
+          <img src={github_icon} alt="github" width={36} height={36} />
+          <p>Link to GitHub</p>
         </a>
-      </section>
+      
     </div>
   );
 }
