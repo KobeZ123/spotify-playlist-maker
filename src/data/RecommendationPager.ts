4768c622baae4248ce6ae9bb97bf73cbf73aa8d4
KobeZ123 <kobezhe@gmail.com>
2023-08-22 22:09:08 -0400

    began working on pager

---
+++ src/data/RecommendationPager.ts
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+import { useRecommendationFetcher } from "./RecommendationsFetcher";
+
+export function useRecommendationPager() {
+  const COUNT_PER_PAGE = 8;
+  const [pager, setPager] = useState<any[][]>([]);
+  const { fetchRecommendations, requestRecommendations } = useRecommendationFetcher(RecommendationType.ARTISTS);
+  useEffect(() => {
+    fetchRecommendations();
+    initializePager();
+  }, []);
+
+  const initializePager = () => {
+    let requestedRecommendations = requestRecommendations(COUNT_PER_PAGE);
+    if (requestedRecommendations != null) {
+      pager.push(requestedRecommendations);
+    }
+    setPager(pager);
+    console.log(requestedRecommendations);
+  }
+}
